af9c66a3c2a7c85a80185a0e9a415436dd818a95
Alfredo <alfredodomin91@gmail.com>
2020-01-06 21:53:13 -0600

    updated server to include API Routes

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
-var fs = require('fs');
+var express = require ('express');
+var path = require ('path');
+var fs = require ('fs');
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000; // Heroku Port formatt
-var notesSaved = require('./db/db.json')
+var notesSavedArr = require('./db/db.json')
 
 // Set up miiddleware to handle data parsing
 app.use(express.static(__dirname + '/public'));
@@ -20,16 +20,33 @@ app.use(express.json());
 // =============================================================
 // Basic route that sends the user first two AJAX request for html files
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-  });
+app.get("/", function(request, response) {
+    response.sendFile(path.join(__dirname, "public/index.html"));
+});
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-  });
+app.get("/notes", function(request, response) {
+    response.sendFile(path.join(__dirname, "public/notes.html"));
+});
   
+// API Routes
+//Displays all notes Saved in obj
+
+app.get("/api/notes", function (request, response) {
+    response.json(notesSavedArr);
+});
+
+app.post("/api/notes", function (request, response) {
+    var newNote = request.body;
+    // console.log(newNote)
+    notesSavedArr.push(newNote);
+    console.log(notesSavedArr)
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesSavedArr, null, 2), "utf-8")
+    response.json(newNote);
+});
 
 
-  app.listen(PORT, function() {
+
+
+app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
     });
